01e1d911d98e41f55abe685762db0f394b1c8d66
James Duot <duot.jim@gmail.com>
2021-07-27 11:42:29 -0700

    feat: set default port to 9001

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("colors");
 const express = require("express");
 const morgan = require("morgan");
 
+const PORT = process.env.PORT || 9001;
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
@@ -10,44 +11,52 @@ app.use(express.json());
 const triplets = [];
 
 app.post("/triplets", (req, res) => {
-  const data = req.body
-  triplets.push(data)
+  const data = req.body;
+  triplets.push(data);
   res.end();
-})
+});
 
 app.get("/triplets", (req, res) => {
-  res.json(triplets)
-})
+  res.json(triplets);
+});
 
 app.get("/deltas", (req, res) => {
-  const Diff = require('diff')
-  const { pruneResponse, convertBodyToText, isDifferentBody, correlationIdOf } = require('./src/utils')
-  const statusAndBodyDiff = triplets.filter(({ request, response, replayedResponse }) => {
-    if (response.status !== replayedResponse.status) return true;
+  const Diff = require("diff");
+  const {
+    pruneResponse,
+    convertBodyToText,
+    isDifferentBody,
+    correlationIdOf,
+  } = require("./src/utils");
+  const statusAndBodyDiff = triplets.filter(
+    ({ request, response, replayedResponse }) => {
+      if (response.status !== replayedResponse.status) return true;
 
-    if (isDifferentBody(response, replayedResponse)) return true;
+      if (isDifferentBody(response, replayedResponse)) return true;
 
-    return false
-  })
+      return false;
+    }
+  );
 
-  const listOfDeltas = statusAndBodyDiff.map(({ response, replayedResponse }) => {
-    const correlationId = correlationIdOf(response)
-    const recorded = convertBodyToText(pruneResponse(response))
-    const replayed = convertBodyToText(pruneResponse(replayedResponse))
-    const delta = Diff.diffJson(recorded, replayed)
+  const listOfDeltas = statusAndBodyDiff.map(
+    ({ response, replayedResponse }) => {
+      const correlationId = correlationIdOf(response);
+      const recorded = convertBodyToText(pruneResponse(response));
+      const replayed = convertBodyToText(pruneResponse(replayedResponse));
+      const delta = Diff.diffJson(recorded, replayed);
 
-    delta.forEach((part) => {
-      const color = part.added ? 'green' :
-        part.removed ? 'red' : 'grey';
-      process.stderr.write(part.value[color]);
-    })
+      delta.forEach((part) => {
+        const color = part.added ? "green" : part.removed ? "red" : "grey";
+        process.stderr.write(part.value[color]);
+      });
 
-    return { correlationId, delta }
-  })
-  res.json(listOfDeltas)
-})
+      return { correlationId, delta };
+    }
+  );
+  res.json(listOfDeltas);
+});
 
-app.listen(process.env.PORT, (err) => {
+app.listen(PORT, (err) => {
   if (err) console.error(err);
-  console.log("Server is listening on PORT ", process.env.PORT);
+  console.log("Server is listening on PORT ", PORT);
 });
