2da7de73d891a4ca3f9b559700655c8748af376a
James Duot <duot.jim@gmail.com>
2021-07-28 05:39:29 -0700

    feat: replayedResponse model

---
+++ db/models/replayedResponse.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ReplayedResponseSchema = new Schema({
+  replaySessionId: Number,
+  correlationId: String,
+  status: Number,
+  headers: [{ header: String }],
+  body: Buffer,
+  meta: {
+    request_type: String,
+    request_id: String,
+    timestamp: Number,
+    latency: Number,
+  },
+});
+
+const ReplayedResponse = mongoose.model("ReplayedResponse", ReplayedResponseSchema);
+module.exports = { ReplayedResponse, ReplayedResponseSchema };
